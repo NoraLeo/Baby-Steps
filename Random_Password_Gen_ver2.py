5656cfb2d5314486018a19a773990563381c8627
NoraLeo <38717394+NoraLeo@users.noreply.github.com>
2020-11-29 12:55:48 +0800

    Create Random_Password_Gen_ver2.py
    This is a simpler method for Random_Password_Gen.py.

---
+++ Random_Password_Gen_ver2.py
@@ -0,0 +1,13 @@
+# a shorter method
+
+#creating an empty list
+passwd=[]
+
+#doing a loop to generate a random letter and add to the empty list twelve times
+
+for i in range(13): #making sure that it meets intl password reqs
+  import random
+  passwd.append(random.choice(charlist))
+
+#display password
+print("Your password is:", passwd)
