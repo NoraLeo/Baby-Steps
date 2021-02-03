5f66b70a0fce1fbb52933bc9550ee6be17e151af
Dheena  Leonora <dheenaleo2512@gmail.com>
2021-02-03 09:27:40 +0000

    added a tiny project, sory_generator

---
+++ story_creator.py
@@ -0,0 +1,19 @@
+#putting my Python skills to test
+
+loop= 1
+while (loop<10):
+  char1= input('Enter name of first character: ')
+  char2= input('Enter name of second character: ')
+  bad_guy= input('Enter the name of the villain: ')
+  reason= input('Why was the bad guy not happy?: ')
+  place= input('Where did this happen?: ')
+  rel= input('How they were to each other: ')
+  inc= input('What did the villain do to them?: ')
+  sol= input('Solution?: ')
+  end= input('Ending?: ')
+#A sample Story  
+  print('---------------------------------------------------------------------------------------------------')
+  print('Long time ago, in a', place, 'there was', char1, 'and', char2+'. They were good', rel, 'and they never seperated from each other. Suddenly, one day, a bad, bad,', bad_guy, 'came to make their lives difficult, as', reason, '. So he', inc, 'and the two of them became sad. They stopped for a moment, then thought hard, and guess what they did?', sol, '! Then the bad person became sad, and', end +'.')
+
+  loop= loop + 1
+
